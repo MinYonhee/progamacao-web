9050324425e85039df094f84189aa05f8af80407
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2024-08-30 22:28:46 +0000

    atualizacoes no codigo

--- ex01/soma.js
+++ ex01/soma.js
@@ -1,14 +1,21 @@
 let sum = 0;
+let count = 0;
 
 function addNumber() {
     const numberInput = document.getElementById('numberInput');
     const sumResult = document.getElementById('sumResult');
+    const media = document.getElementById('media');
+    const qtdNum = document.getElementById('qtdNum');
     
     const number = parseFloat(numberInput.value);
     
     if (!isNaN(number)) {
         sum += number;
+        count++;
+        
         sumResult.value = sum;
+        media.value = (sum / count).toFixed(2); // Calcula a média com 2 casas decimais
+        qtdNum.value = count;
     }
     
     numberInput.value = '';  
